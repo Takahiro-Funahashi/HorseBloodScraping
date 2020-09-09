631e9885d6401ca0204558b2e82f017a58d5d5b0
t-funahashi <t-funahashi@imagenics.co.jp>
2020-09-09 14:24:40 +0900

    Ver.0.1.001

---
+++ HorseBloodSQLite.py
@@ -0,0 +1,246 @@
+import copy
+import os
+import pathlib
+import sqlite3
+
+unknown_mac = 'ffffffffffff'
+primaryTbl = None
+not_null = 'NOT NULL'
+primary_key = 'PRIMARY KEY'
+auto_increment = 'AUTOINCREMENT'
+uniaue = 'UNIQUE'
+default = 'DEFAULT'
+null = 'NULL'
+foreign_key = 'FOREIGN KEY'
+referances = 'REFERENCES'
+insert = 'INSERT INTO'
+select = 'SELECT'
+update = 'UPDATE'
+delete = 'DELETE'
+
+HorseNameTbl = 'HorseNameTbl'
+HorseBloodTbl = 'HorseBloodTbl'
+RequestTbl = 'RequestTbl'
+
+class class_SQLite():
+    def __init__(self):
+        path = os.getcwd()
+        path = path + '\\data'
+        path = os.path.split(path)
+        path = path[0]
+        ret = pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+        self.database_name = path + '\\HorseBlood.db'
+        self.link_dict_after = dict()
+        return
+
+    def set_link_dict(self,link_dict):
+        self.link_dict_after = copy.deepcopy(link_dict)
+
+    def connect_DB(self):
+        self.DBconnect = sqlite3.connect(self.database_name)
+        self.DBcursor = self.DBconnect.cursor()
+        return
+
+    def disconnect_DB(self):
+        self.DBconnect.close()
+
+    def commit_DB(self):
+        self.DBconnect.commit()
+
+    def create_Horse_Tbl(self):
+        sql_cmd = f'CREATE TABLE IF NOT EXISTS "{HorseNameTbl}" '
+        sql_cmd = sql_cmd + \
+            f'("pkey" INTEGER {not_null} {primary_key} {auto_increment} {uniaue}'
+        sql_cmd = sql_cmd + f',"Name" TEXT {not_null}'
+        sql_cmd = sql_cmd + f',"Birthday" TEXT {not_null}'
+        sql_cmd = sql_cmd + f',"URL" TEXT )'
+
+        self.DBcursor.execute(sql_cmd)
+
+        return
+
+    def create_HorseBlood_Tbl(self):
+        sql_cmd = f'CREATE TABLE IF NOT EXISTS "{HorseBloodTbl}" '
+        sql_cmd = sql_cmd + \
+            f'("pkey" INTEGER {not_null} {primary_key} {auto_increment} {uniaue}'
+        sql_cmd = sql_cmd + f',"horse_pkey" INTEGER {not_null}'
+        sql_cmd = sql_cmd + f',"f" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"m" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"ff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"ffm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mfm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"ffff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fffm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"ffmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"ffmm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmfm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"fmmm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mfff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mffm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mfmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mfmm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmff" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmfm" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmmf" INTEGER {default} {null}'
+        sql_cmd = sql_cmd + f',"mmmm" INTEGER {default} {null})'
+
+        self.DBcursor.execute(sql_cmd)
+
+        return
+
+    def create_Request_Tbl(self):
+        sql_cmd = f'CREATE TABLE IF NOT EXISTS "{RequestTbl}" '
+        sql_cmd = sql_cmd + \
+            f'("pkey" INTEGER {not_null} {primary_key} {auto_increment} {uniaue}'
+        sql_cmd = sql_cmd + f',"Name" TEXT {not_null}'
+        sql_cmd = sql_cmd + f',"URL" TEXT '
+        sql_cmd = sql_cmd + f',"horse_pkey" INTEGER {default} {null})'
+
+        self.DBcursor.execute(sql_cmd)
+
+    def replace_Horse_Tbl(self,HorseName,Birthday,url,horse_pkey):
+        if Birthday is None:
+            Birthday = f'{null}'
+        if url is None:
+            url = f'{null}'
+
+        if horse_pkey is None:
+            sql_cmd = f'{select} * FROM {HorseNameTbl} WHERE Name = "{HorseName}" AND URL = "{url}"'
+        else:
+            sql_cmd = f'{select} * FROM {HorseNameTbl} WHERE pkey = "{horse_pkey}"'
+        self.DBcursor.execute(sql_cmd)
+        TblInfo = self.DBcursor.fetchall()
+
+        if TblInfo:
+            pkey = TblInfo[0][0]
+            if TblInfo[0][2] != 'NULL' and TblInfo[0][2]:
+                Birthday = TblInfo[0][2]
+            sql_cmd = f'{update} {HorseNameTbl} SET (Name,Birthday,URL) = ("{HorseName}","{Birthday}","{url}") WHERE pkey={pkey}'
+            self.DBcursor.execute(sql_cmd)
+        else:
+            sql_cmd = f'{insert} {HorseNameTbl} (Name,Birthday,URL) VALUES ("{HorseName}","{Birthday}","{url}")'
+            self.DBcursor.execute(sql_cmd)
+
+            sql_cmd = f'{select} pkey FROM {HorseNameTbl} WHERE rowid = last_insert_rowid()'
+            self.DBcursor.execute(sql_cmd)
+
+            TblInfo = self.DBcursor.fetchall()
+            if TblInfo:
+                pkey = TblInfo[0][0]
+
+        return pkey
+
+    def replace_HorseBoold_Tbl(self,HorseName,Birthday,horse_blood,link_dict,url,horse_pkey):
+
+        if horse_pkey is None:
+            sql_cmd = f'{select} * FROM {HorseNameTbl} WHERE Name = "{HorseName}" AND URL = "{url}"'
+        else:
+            sql_cmd = f'{select} * FROM {HorseNameTbl} WHERE pkey = "{horse_pkey}" '
+
+        self.DBcursor.execute(sql_cmd)
+        TblInfo = self.DBcursor.fetchall()
+
+        if TblInfo:
+            horse_pkey = TblInfo[0][0]
+
+            set_param = ''
+
+            for horse_name in horse_blood:
+                horse_name = horse_name.replace(' ','_')
+                URL, hPkey = link_dict[horse_name]
+                set_param += f'{hPkey},'
+            else:
+                set_param = set_param.rstrip(',')
+
+            sql_cmd = f'{select} pkey FROM {HorseBloodTbl} WHERE horse_pkey = {horse_pkey}'
+            self.DBcursor.execute(sql_cmd)
+            TblInfo = self.DBcursor.fetchall()
+            if TblInfo:
+                pkey = TblInfo[0][0]
+                sql_cmd = f'{update} {HorseBloodTbl} SET '\
+                    + f'(f,m,ff,fm,mf,mm)=({set_param}) WHERE horse_pkey={horse_pkey}'
+            else:
+                sql_cmd = f'{insert} {HorseBloodTbl} '\
+                    + f'(horse_pkey,f,m,ff,fm,mf,mm) VALUES ({horse_pkey},{set_param})'
+
+            self.DBcursor.execute(sql_cmd)
+
+        return
+
+    def replace_request_Tbl(self,link_dict):
+
+        add_dict = dict()
+        remove_dict = dict()
+
+        for k, v in link_dict.items():
+            if k not in self.link_dict_after:
+                add_dict.setdefault(k, v)
+
+        for k, v in self.link_dict_after.items():
+            if k not in link_dict:
+                remove_dict.setdefault(k, v)
+
+        self.link_dict_after = copy.deepcopy(link_dict)
+
+        tbl_name = 'RequestTbl'
+
+        for k, v in add_dict.items():
+            url, key = v
+            #sql_cmd = f'{select} * FROM {tbl_name} WHERE Name = "{k}" AND URL = "{v}"'
+            sql_cmd = f'{select} * FROM {tbl_name} WHERE Name = "{k}" AND horse_pkey = {key}'
+            self.DBcursor.execute(sql_cmd)
+            TblInfo = self.DBcursor.fetchall()
+            if TblInfo:
+                pkey = TblInfo[0][0]
+                sql_cmd = f'{update} {tbl_name} SET (Name,URL,horse_pkey) = ("{k}","{url}",{key}) WHERE pkey={pkey}'
+            else:
+                sql_cmd = f'{insert} {tbl_name} (Name,URL,horse_pkey) VALUES ("{k}","{url}",{key})'
+
+            self.DBcursor.execute(sql_cmd)
+
+        for k, v in remove_dict.items():
+            url, key = v
+            sql_cmd = f'{delete} FROM {tbl_name} WHERE Name = "{k}" AND horse_pkey = {key}'
+            self.DBcursor.execute(sql_cmd)
+
+        return
+
+    def get_request_Tbl(self):
+        self.connect_DB()
+
+        tbl_name = 'RequestTbl'
+        sql_cmd = f'{select} Name,URL,horse_pkey FROM {tbl_name}'
+        self.DBcursor.execute(sql_cmd)
+        TblInfo = self.DBcursor.fetchall()
+        if TblInfo:
+            for Name,URL,horse_pkey in TblInfo:
+                self.link_dict_after.setdefault(Name,[URL,horse_pkey])
+
+        self.disconnect_DB()
+
+        return self.link_dict_after
+
+    def get_HorseURL_Tbl(self,url):
+        self.connect_DB()
+
+        sql_cmd = f'{select} * FROM {HorseNameTbl} WHERE URL = "{url}" AND NOT Birthday = "{null}"'
+        self.DBcursor.execute(sql_cmd)
+        TblInfo = self.DBcursor.fetchall()
+
+        self.disconnect_DB()
+
+        if TblInfo:
+            return True
+        else:
+            return False
---
+++ HorseBloodScraping.py
@@ -0,0 +1,210 @@
+import cchardet
+from bs4 import BeautifulSoup
+
+from fake_useragent import UserAgent
+
+import requests
+
+import time
+
+import HorseBloodSQLite as HBDB
+
+class HorseData_scraping():
+    def __init__(self,interval_time=1,limit_time = 600):
+        self.time_out = 3
+        ua = UserAgent()
+        self.header = {'user-agent':ua.chrome}
+        if interval_time >= 1:
+            self.interval_time = interval_time
+        else:
+            self.interval_time = 1
+
+        if limit_time > 0 and limit_time <= 3600:
+            self.limit_time = limit_time
+        else:
+            self.limit_time = 600
+
+        return
+
+    def get_response(self,url):
+        response = requests.get(url,headers=self.header,timeout=self.time_out)
+        response.encoding = response.apparent_encoding
+        enc = cchardet.detect(response.content)
+        encoding = enc['encoding']
+
+        return BeautifulSoup(response.text, "html.parser")
+
+    def get_HorseName(self, soup):
+        # タイトルから馬名を取得
+        _title_ = soup.find('title').get_text(strip=True)
+        _title_ = _title_.strip(' - netkeiba.com')
+        t = _title_.split('|')
+        horse_name = t[0].rstrip(' ')
+
+        return horse_name
+
+    def get_HorseProfile(self, soup):
+        # プロフィールテーブルを取得
+        horse_profile =soup.find("table",class_="db_prof_table no_OwnerUnit").get_text()
+        horse_profile = horse_profile.split()
+
+        return horse_profile
+
+    def get_HorseBirthday(self, horse_profile):
+        horse_birthday = None
+
+        profile_iter = iter(horse_profile)
+
+        while(True):
+            try:
+                ans = next(profile_iter)
+                if ans == '生年月日':
+                    horse_birthday =next(profile_iter)
+                    break
+            except StopIteration:
+                break
+
+        return horse_birthday
+
+    def get_HorseBoold(self, soup):
+
+        horse_blood =soup.find("table",class_="blood_table").get_text()
+        horse_blood = horse_blood.split('\n')
+        horse_blood = [h for h in horse_blood if len(h)>0]
+
+        f_horse,ff_horse,fm_horse,m_horse,mf_horse,mm_horse = horse_blood
+
+        return horse_blood
+
+    def get_NextHorseLink(self, soup, horse_blood):
+        link_dict = dict()
+        for horse_name in horse_blood:
+            link =soup.find("a",text=horse_name)
+            url = link.attrs['href']
+            url = "https://db.netkeiba.com" + url
+            url = self.get_NextHorseInfo(horse_name,url)
+            link_dict.setdefault(horse_name,url)
+
+        return link_dict
+
+    def get_NextHorseInfo(self,horse_name,url):
+        soup = self.get_response(url)
+        print('access:%-16s:%s' % (horse_name,url))
+        horse_menu =soup.find("a",title=f'{horse_name}のプロフィールTOP')
+        url = horse_menu.attrs['href']
+        url = "https://db.netkeiba.com" + url
+
+        return url
+
+    def get_HorseInfo(self,url,horse_pkey,u_link_dict):
+        soup = self.get_response(url)
+        horse_name = self.get_HorseName(soup)
+
+        print('access:%-16s:%s' % (horse_name,url))
+        horse_name = horse_name.replace(' ','_')
+        horse_profile = self.get_HorseProfile(soup)
+        horse_birthday = self.get_HorseBirthday(horse_profile)
+        horse_blood = self.get_HorseBoold(soup)
+        horse_blood_link_dict = self.get_NextHorseLink(soup,horse_blood)
+
+        self.DB.connect_DB()
+
+        link_dict = dict()
+
+        horse_pkey = self.DB.replace_Horse_Tbl(horse_name,horse_birthday,url,horse_pkey)
+        for b_horse_name, b_url in horse_blood_link_dict.items():
+            b_horse_name = b_horse_name.replace(' ','_')
+            ｂ_horse_pkey = self.DB.replace_Horse_Tbl(b_horse_name,None,b_url,None)
+            link_dict.setdefault(b_horse_name,[b_url,ｂ_horse_pkey])
+
+        self.DB.replace_HorseBoold_Tbl(horse_name,horse_birthday,horse_blood,link_dict,url,horse_pkey)
+
+        u_link_dict.update(link_dict)
+
+        self.DB.replace_request_Tbl(u_link_dict)
+
+        self.DB.commit_DB()
+        self.DB.disconnect_DB()
+
+
+        f_horse,ff_horse,fm_horse,m_horse,mf_horse,mm_horse = horse_blood
+
+        print('馬名',horse_name)
+        print('生年月日',horse_birthday)
+        print('父',f_horse)
+        print('父父',ff_horse)
+        print('父母',fm_horse)
+        print('母',m_horse)
+        print('母父',mf_horse)
+        print('母母',mm_horse)
+
+        return u_link_dict
+
+    def run(self, url):
+        horse_pkey = None
+        link_dict = dict()
+
+        self.DB = HBDB.class_SQLite()
+        self.DB.connect_DB()
+        self.DB.create_Horse_Tbl()
+        self.DB.create_HorseBlood_Tbl()
+        self.DB.create_Request_Tbl()
+        self.DB.commit_DB()
+        self.DB.disconnect_DB()
+
+        # 前回の続きのリクエストリストを取得
+        link_dict = self.DB.get_request_Tbl()
+
+        def get_next(link_dict):
+            next_horse = None
+            ans = list(link_dict.keys())
+            if ans:
+                next_horse = ans[0]
+                url = link_dict[next_horse][0]
+                pkey = link_dict[next_horse][1]
+                link_dict.pop(next_horse)
+
+            return next_horse,url,pkey
+
+        if link_dict:
+            self.DB.set_link_dict(link_dict)
+            next_horse,url,horse_pkey = get_next(link_dict)
+
+        print('スクレイピングを開始します。')
+
+        start_time = time.time()
+
+        horse_name = dict()
+
+        while(True):
+            #time.sleep(self.interval_time)
+            link_dict = self.get_HorseInfo(url,horse_pkey,link_dict)
+            if len(link_dict) == 0:
+                break
+            while(True):
+                next_horse, url, horse_pkey = get_next(link_dict)
+                if not self.DB.get_HorseURL_Tbl(url):
+                    break
+                print(next_horse,'既に登録されているため、スキップします。')
+
+            print(len(link_dict))
+
+            if next_horse in horse_name:
+                horse_name[next_horse] = horse_name[next_horse] +1
+            else:
+                horse_name.setdefault(next_horse,1)
+
+            ntime = time.time()
+
+            if ntime - start_time > self.limit_time:
+                print(f'規定時間{self.limit_time}秒を経過したため、スクレイピングを終了しします。')
+                print(horse_name)
+                break
+
+        return
+
+if __name__ == '__main__':
+    start_url = "https://db.netkeiba.com/horse/2002100816/"
+    hScr = HorseData_scraping(limit_time=3000)
+    hScr.run(start_url)
+
